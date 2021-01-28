d33a857a88c3e34c955ca15e9c2f3fbb0c85d2da
raksa <eng.raksa@gmail.com>
2021-01-28 16:38:44 +0700

    update

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 # raw compile
 # clang++ -w -framework glut -framework opengl src/main_opengl.cpp -o main
 
-cmake CMakeLists.txt && make && ./main ../Battambang-Regular.ttf "សោះស្តី" "l" "km"
+cmake CMakeLists.txt && make && ./main ./assets/fonts/Battambang-Regular.ttf "សោះស្តី" "l" "km"
