4049777712a2ca8736266c85fc9329ed0aabde02
raksa <eng.raksa@gmail.com>
2021-01-30 21:17:20 +0700

    update

--- src/funs.cpp
+++ src/funs.cpp
@@ -1,7 +1,5 @@
-// https://www.opengl.org/archives/resources/code/samples/glut_examples/examples/bitfont.c
-// https://github.com/themattinthehatt/tutorials/tree/master/opengl-tutorial/playground
+// http://madig.github.io/freetype-web/documentation/tutorial/
 // https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Text_Rendering_01
-// https://gitlab.com/wikibooks-opengl/modern-tutorials
 #include <GLUT/glut.h>
 #include <string.h>
 
@@ -13,7 +11,8 @@ extern "C"
 }
 
 FT_Library ft;
-FT_Face face;
+FT_Face defaultFontFace;
+FT_Face enFontFace;
 GLuint program;
 GLint attribute_coord;
 GLint uniform_tex;
@@ -26,8 +25,9 @@ struct point
   GLfloat s;
   GLfloat t;
 };
-// const char *fontfilename = "/Users/raksa/Desktop/dev/opengl-raqm/assets/fonts/Battambang-Regular.ttf";
-const char *fontfilename = "/Users/raksa/Desktop/dev/opengl-raqm/assets/fonts/POPPINS-BOLD.TTF";
+bool isKhmer = true;
+const char *kmFontfilename = "/Users/raksa/Desktop/dev/opengl-raqm/assets/fonts/Battambang-Regular.ttf";
+const char *emFontfilename = "/Users/raksa/Desktop/dev/opengl-raqm/assets/fonts/POPPINS-BOLD.TTF";
 const char *vertShader = "/Users/raksa/Desktop/dev/opengl-raqm/assets/shader/vertex.glsl";
 const char *fragShader = "/Users/raksa/Desktop/dev/opengl-raqm/assets/shader/fragment.glsl";
 
@@ -42,13 +42,14 @@ int init_resources()
   }
 
   /* Load a font */
-  if (FT_New_Face(ft, fontfilename, 0, &face))
+  const char *fontfilename = isKhmer ? kmFontfilename : emFontfilename;
+  if (FT_New_Face(ft, fontfilename, 0, &defaultFontFace))
   {
     fprintf(stderr, "Could not open font %s\n", fontfilename);
     return 0;
   }
 
-  FT_Set_Pixel_Sizes(face, 0, 48);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 48);
 
   program = loadShader(vertShader, fragShader);
 
@@ -89,7 +90,7 @@ void free_resources()
   glDeleteProgram(program);
 }
 
-void draw_text(const char *text, float x, float y, float sx, float sy)
+void draw_text(FT_Face *fontFace, const char *text, float x, float y, float sx, float sy)
 {
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -118,13 +119,20 @@ void draw_text(const char *text, float x, float y, float sx, float sy)
   glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
   /* Loop through all characters */
-  FT_GlyphSlot glyph = face->glyph;
+  size_t count, i;
+  FT_GlyphSlot glyph = (*fontFace)->glyph;
+  raqm_glyph_t *raqmGlyphs = getGlyphs(fontFace, &count, text, "l", "km");
   const char *p;
-  for (p = text; *p; p++)
+  for (i = 0; i < count; i++)
   {
-    if (FT_Load_Char(face, *p, FT_LOAD_RENDER))
+    if (FT_Load_Glyph(*fontFace, raqmGlyphs[i].index, FT_LOAD_DEFAULT))
     {
-      fprintf(stderr, "Could not load character 'X'\n");
+      fprintf(stderr, "Could not load glyph\n");
+      return;
+    }
+    if (FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL))
+    {
+      fprintf(stderr, "Could not render glyph\n");
       return;
     }
     /* Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
@@ -136,73 +144,6 @@ void draw_text(const char *text, float x, float y, float sx, float sy)
     float y2 = -y - glyph->bitmap_top * sy;
     float w = glyph->bitmap.width * sx;
     float h = glyph->bitmap.rows * sy;
-
-    point box[4] = {
-        {x2, -y2, 0, 0},
-        {x2 + w, -y2, 1, 0},
-        {x2, -y2 - h, 0, 1},
-        {x2 + w, -y2 - h, 1, 1},
-    };
-
-    /* Draw the character on the screen */
-    glBufferData(GL_ARRAY_BUFFER, sizeof box, box, GL_DYNAMIC_DRAW);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-    /* Advance the cursor to the start of the next character */
-    x += (glyph->advance.x >> 6) * sx;
-    y += (glyph->advance.y >> 6) * sy;
-  }
-
-  glDisableVertexAttribArray(attribute_coord);
-  glDeleteTextures(1, &tex);
-}
-
-void draw_text_raqm(const char *text, float x, float y, float sx, float sy)
-{
-  /* Create a texture that will be used to hold one "glyph" */
-  GLuint tex;
-  size_t count, i;
-  raqm_glyph_t raqmGlyph;
-  FT_Bitmap bitmap;
-  FT_GlyphSlot glyph;
-  raqm_glyph_t *raqmGlyphs = getGlyphs(&face, &count, text, "l", "km");
-
-  glActiveTexture(GL_TEXTURE0);
-  glGenTextures(1, &tex);
-  glBindTexture(GL_TEXTURE_2D, tex);
-  glUniform1i(uniform_tex, 0);
-
-  /* We require 1 byte alignment when uploading texture data */
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-  /* Clamping to edges is important to prevent artifacts when scaling */
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-  /* Linear filtering usually looks best for text */
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  /* Set up the VBO for our vertex data */
-  glEnableVertexAttribArray(attribute_coord);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
-  /* Loop through all characters */
-  for (i = 0; i < count; i++)
-  {
-    raqmGlyph = raqmGlyphs[i];
-    bitmap = raqmGlyph.ftface->glyph->bitmap;
-    glyph = raqmGlyph.ftface->glyph;
-    /* Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, bitmap.width, bitmap.rows,
-                 0, GL_RED, GL_UNSIGNED_BYTE, bitmap.buffer);
-
-    /* Calculate the vertex and texture coordinates */
-    float x2 = x + glyph->bitmap_left * sx;
-    float y2 = -y - glyph->bitmap_top * sy;
-    float w = bitmap.width * sx;
-    float h = bitmap.rows * sy;
 
     point box[4] = {
         {x2, -y2, 0, 0},
@@ -240,31 +181,52 @@ void apply_draw_text()
   GLfloat transparent_green[4] = {0, 1, 0, 0.5};
 
   /* Set font size to 48 pixels, color to black */
-  FT_Set_Pixel_Sizes(face, 0, 48);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 48);
   glUniform4fv(uniform_color, 1, black);
 
   /* Effects of alignment */
-  draw_text("The Quick Brown Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 50 * sy, sx, sy);
-  draw_text("The Misaligned Fox Jumps Over The Lazy Dog", -1 + 8.5 * sx, 1 - 100.5 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងពណ៌ត្នោតរហ័សលោតលើឆ្កែខ្ជិល" : "The Quick Brown Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 50 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងឆ្កែច្របូកច្របល់លោតលើឆ្កែខ្ជិល" : "The Misaligned Fox Jumps Over The Lazy Dog",
+            -1 + 8.5 * sx, 1 - 100.5 * sy, sx, sy);
 
   /* Scaling the texture versus changing the font size */
-  draw_text("The Small Texture Scaled Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 175 * sy, sx * 0.5, sy * 0.5);
-  FT_Set_Pixel_Sizes(face, 0, 24);
-  draw_text("The Small Font Sized Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 200 * sy, sx, sy);
-  FT_Set_Pixel_Sizes(face, 0, 48);
-  draw_text("The Tiny Texture Scaled Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 235 * sy, sx * 0.25, sy * 0.25);
-  FT_Set_Pixel_Sizes(face, 0, 12);
-  draw_text("The Tiny Font Sized Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 250 * sy, sx, sy);
-  FT_Set_Pixel_Sizes(face, 0, 48);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងវាយនភាពខ្នាតតូចលោតលើឆ្កែខ្ជិល" : "The Small Texture Scaled Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 175 * sy, sx * 0.5, sy * 0.5);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 24);
+  draw_text(&defaultFontFace, isKhmer ? "ហ្វកតូចខ្នាតតូចលោតលើឆ្កែខ្ជិល" : "The Small Font Sized Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 200 * sy, sx, sy);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 48);
+  draw_text(&defaultFontFace, isKhmer ? "វាយនភាពខ្នាតតូចធ្វើឱ្យកញ្ជ្រោងលោតលើឆ្កែខ្ជិល" : "The Tiny Texture Scaled Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 235 * sy, sx * 0.25, sy * 0.25);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 12);
+  draw_text(&defaultFontFace, isKhmer ? "ខ្នាតតូចខ្នាតតូចអក្សរហ្វកលោតលើសត្វឆ្កែខ្ជិល" : "The Tiny Font Sized Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 250 * sy, sx, sy);
+  FT_Set_Pixel_Sizes(defaultFontFace, 0, 48);
 
   /* Colors and transparency */
-  draw_text("The Solid Black Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 430 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងខ្មៅរឹងលោតលើឆ្កែខ្ជិល" : "The Solid Black Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 430 * sy, sx, sy);
 
   glUniform4fv(uniform_color, 1, red);
-  draw_text("The Solid Red Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 330 * sy, sx, sy);
-  draw_text("The Solid Red Fox Jumps Over The Lazy Dog", -1 + 28 * sx, 1 - 450 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងក្រហមរឹងលោតលើឆ្កែខ្ជិល" : "The Solid Red Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 330 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "កញ្ជ្រោងក្រហមរឹងលោតលើឆ្កែខ្ជិល" : "The Solid Red Fox Jumps Over The Lazy Dog",
+            -1 + 28 * sx, 1 - 450 * sy, sx, sy);
 
   glUniform4fv(uniform_color, 1, transparent_green);
-  draw_text("The Transparent Green Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 380 * sy, sx, sy);
-  draw_text("The Transparent Green Fox Jumps Over The Lazy Dog", -1 + 18 * sx, 1 - 440 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "សត្វកញ្ជ្រោងបៃតងថ្លាលោតលើឆ្កែខ្ជិល" : "The Transparent Green Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 380 * sy, sx, sy);
+  draw_text(&defaultFontFace, isKhmer ? "សត្វកញ្ជ្រោងបៃតងថ្លាលោតលើឆ្កែខ្ជិល តន្ត្រី" : "The Transparent Green Fox Jumps Over The Lazy Dog",
+            -1 + 18 * sx, 1 - 440 * sy, sx, sy);
+
+  if (FT_New_Face(ft, emFontfilename, 0, &enFontFace))
+  {
+    fprintf(stderr, "Could not open font %s\n", emFontfilename);
+    return;
+  }
+  FT_Set_Pixel_Sizes(enFontFace, 0, 48);
+  glUniform4fv(uniform_color, 1, red);
+  draw_text(&enFontFace, "The Quick Brown Fox Jumps Over The Lazy Dog",
+            -1 + 8 * sx, 1 - 50 * sy, sx, sy);
 }
