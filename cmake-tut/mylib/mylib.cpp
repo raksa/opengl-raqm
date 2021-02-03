ed74c968e6e21210e6f8ce41eab32c224c558be3
raksa <eng.raksa@gmail.com>
2021-02-03 14:13:01 +0700

    update

--- cmake-tut/mylib/mylib.cpp
+++ cmake-tut/mylib/mylib.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include "Table.h"
+
 void say(std::string str)
 {
 #ifndef SHOULD_NOT_SAY
+    std::cout << sqrtTable[0] << sqrtTable[1] << sqrtTable[2] << sqrtTable[3] << std::endl;
     std::cout << str << std::endl;
 #endif
 }
